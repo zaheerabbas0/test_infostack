eb4a971b245baf3bc736ed1339e63efe25a142ef
Zaheer <za00901@gmail.com>
2025-03-06 22:25:56 +0500

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get "notes/index"
-  get "notes/new"
-  get "notes/create"
-  get "users/new"
-  get "users/create"
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "sessions#new"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+
+  resources :notes, only: [:index, :new, :create]
 end
