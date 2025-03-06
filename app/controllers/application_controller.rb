26cf8d3ae773561df278182f0237585b76c25d90
Zaheer <za00901@gmail.com>
2025-03-06 22:16:01 +0500

    Implemented Private Route in applicaiton controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+   def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user!
+    redirect_to login_path, alert: "You must be logged in to access this page" unless current_user
+  end
 end
